6e756b5dce719a46b40219cdee39b2643ebf602f
Cristian Mellibosky <cristian.mellibosky@darwoft.com>
2020-04-29 10:44:12 -0300

    se agregaron repositorios de product y category

---
+++ ViajesAPI/Repositories/Implementations/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViajesAPI.Models.Contexts;
+using ViajesAPI.Models.Entities;
+using ViajesAPI.Repositories.Interfaces;
+
+namespace ViajesAPI.Repositories.Implementations
+{
+    public class CategoryRepository: GenericRepository<Category> , ICategoryRepository
+    {
+        public CategoryRepository(Context context) : base(context) { }
+    }
+}
---
+++ ViajesAPI/Repositories/Implementations/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ViajesAPI.Models.Contexts;
+using ViajesAPI.Models.Entities;
+using ViajesAPI.Repositories.Interfaces;
+
+namespace ViajesAPI.Repositories.Implementations
+{
+    public class ProductRepository: GenericRepository<Product> , IProdcutRepository
+    {
+      
+        public ProductRepository(Context context) : base(context) { }
+    }
+}
